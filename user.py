35ed85b6399eaba5c16a98a5aa2fd185cfaa5aa6
noa <aliardakani78@gmail.com>
2022-06-07 17:27:25 +0430

    Fix leverage and margin type

--- user.py
+++ user.py
@@ -243,7 +243,7 @@ class User(Client, Strategy):
             if strategy.start_trade and strategy.data.date.iloc[-1] == current_candle["date"]:
                 # If there is no open position, then open position (Only used for having 1 open position at the same time)
                 if strategy.open_positions == []:
-                    quantity = float(str(strategy.free_secondary * percent_of_assets * 0.997 / current_candle["close"])[:4])
+                    quantity = float(str(strategy.free_secondary * percent_of_assets * 0.997 / current_candle["close"])[:5])
                     print(strategy.free_secondary * percent_of_assets * 0.997 / current_candle["close"])
                     if direction == "long":
                         side = "BUY"
